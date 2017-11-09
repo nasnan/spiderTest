0d0b6d10d60a232d51335160f1629331cdb09437
Zoey <nasnan77@outlook.com>
2017-11-09 19:52:02 +0800

    存到数据库

--- doubanFilmTop250.py
+++ doubanFilmTop250.py
@@ -7,8 +7,20 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pymysql
+
+conn = pymysql.connect("localhost","root","a742695",db = "mysql",charset="utf8")
+cur = conn.cursor()
+cur.execute("use scraping")
+
+
 
 url = 'https://movie.douban.com/top250'
+
+
+def store(name,content,rate):
+    cur.execute("insert into doubantop250 (name,content,rate) values('%s','%s','%s')" %(name,content,rate))
+    cur.connection.commit()
 
 def getNextUrl(startPage):
     global url
@@ -31,14 +43,23 @@ def getData(bsObj):
             inq = item.find("span",{"class":"inq"}).get_text()
         except AttributeError as e:
             inq = '无短评'
+        inq = inq.replace("'","\\\'")
+        inq = inq.replace('"','\\\"')
         print(title,rating,inq)
+        store(title,inq,rating)
+
+
 
 def followOnePage(startPage):
     html = urlopen(startPage)
-    bsObj = BeautifulSoup(html)
+    bsObj = BeautifulSoup(html,'lxml')
     getData(bsObj)
     nextUrl = getNextUrl(startPage)
     if nextUrl is not None:
         followOnePage(nextUrl)
 
+
 followOnePage(url)
+
+cur.close()
+conn.close()
